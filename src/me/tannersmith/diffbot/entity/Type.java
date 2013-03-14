bb2e628968d491d269b0d4c7c908fd7abf215ed0
Tanner Smith <tanner@tssoftware.net>
2013-03-14 19:17:10 -0400

    Add method of creating a Type from a human-readable name of a type.

--- src/me/tannersmith/diffbot/entity/Type.java
+++ src/me/tannersmith/diffbot/entity/Type.java
@@ -9,9 +9,53 @@ public enum Type {
 	/**
 	 * Image type
 	 */
-	IMAGE,
+	IMAGE("image"),
 	/**
 	 * Video type
 	 */
-	VIDEO
+	VIDEO("video"),
+	/**
+	 * Unknown type
+	 */
+	UNKNOWN("unknown");
+	
+	/**
+	 * Human-readable name of the Type
+	 */
+	private final String name;
+	
+	/**
+	 * Create a Type with the given name.
+	 * 
+	 * @param name Human-readable name of the Type
+	 */
+	private Type(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * @return Human-readable name of the Type
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * Get the Type type from the human-readable name.
+	 * 
+	 * @param name Human-readable name of a type
+	 * 
+	 * @return Type corresponding to the given name
+	 */
+	public static Type getTypeFromName(String name) {
+		name = name.toLowerCase();
+		
+		if (name.equals(IMAGE.name)) {
+			return IMAGE;
+		} else if (name.equals(VIDEO.name)) {
+			return VIDEO;
+		} else {
+			return UNKNOWN;
+		}
+	}
 }
