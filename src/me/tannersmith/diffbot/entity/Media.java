7f2cb0545b1ec08191c1f48e2a11f6e4e268df33
Tanner Smith <tanner@tssoftware.net>
2013-03-14 19:30:53 -0400

    Parse JSON for media items.

--- src/me/tannersmith/diffbot/entity/Media.java
+++ src/me/tannersmith/diffbot/entity/Media.java
@@ -1,5 +1,11 @@
 package me.tannersmith.diffbot.entity;
 
+import me.tannersmith.diffbot.DiffbotAPIException;
+import me.tannersmith.diffbot.Entity;
+import argo.jdom.JdomParser;
+import argo.jdom.JsonRootNode;
+import argo.saj.InvalidSyntaxException;
+
 /**
  * Class representing a media item that would be within a page.
  * 
@@ -20,6 +26,44 @@ public class Media {
 	 * Link to the media
 	 */
 	private String link;
+	
+	/**
+	 * Construct a new Media.
+	 * 
+	 * @param response Response from the Article API response
+	 * 
+	 * @throws DiffbotAPIException If unable to parse the response
+	 */
+	public Media(String response) throws DiffbotAPIException {
+		parseResponse(response);
+	}
+	
+	/**
+	 * Parses the JSON response from the {@link Article} API response.
+	 * 
+	 * @see Article#parseResponse(String)
+	 * @see Entity#parseResponse(String)
+	 * 
+	 * @throws DiffbotAPIException If unable to parse the response as JSON
+	 */
+	public void parseResponse(String response) throws DiffbotAPIException {
+		JdomParser parser = new JdomParser();
+		JsonRootNode root = null;
+		
+		try {
+			root = parser.parse(response);
+		} catch (InvalidSyntaxException e) {
+			throw new DiffbotAPIException("API did not respond in a format that could be understood");
+		}
+		
+		type = Type.getTypeFromName(root.getStringValue("type"));
+				
+		if (root.isNode("primary")) {
+			primary = root.getBooleanValue("primary");
+		}
+		
+		link = root.getStringValue("link");
+	}
 	
 	/**
 	 * @return Type of media
